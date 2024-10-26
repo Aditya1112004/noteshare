3fe77a8789717456c90cb127b9401c2904a4cb87
Aditya1112004 <adityakumawat2004@gmail.com>
2024-10-26 21:12:35 +0530

    commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://Aak1112004:Aak1112004@cluster0.tft820e.mongodb.net/noteshare?retryWrites=true&w=majority",
+    // "mongodb+srv://Aak1112004:Aak1112004@cluster0.tft820e.mongodb.net/noteshare?retryWrites=true&w=majority",
+    // "mongodb+srv://Aditya11:Aditya1112004@cluster0.78dtulr.mongodb.net/noteshare",
+    "mongodb+srv://Aditya11:Aditya1112004@cluster0.78dtulr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
